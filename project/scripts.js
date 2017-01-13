330cc624aeff6896ee938e1953918faffd4dcecf
ElineJ <elinejacobse@gmail.com>
2017-01-13 16:04:01 +0100

    Added suicide data to world map

--- project/scripts.js
+++ project/scripts.js
@@ -19,12 +19,17 @@ var svg = d3.select("#linecontainer").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// all colors to be used for the data (source: http://colorbrewer2.org/)
+var all_colors = ['#ffffb2','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#b10026'];
+var color_values = [3, 5, 7, 10, 15, 20, 20.1];
+    // 44.6, 40.7, 36.7, 32.7, 28.7, 24.7, 20.8];
+
 // get data from csv file
 d3.csv("data/suicide_by_country.csv", function(error, data) {
     if (error) throw error;
     var dataset = {};
 
-    // set up dictionary with country per_thousands to codes
+    // set up dictionary with country names to country codes
     var codes = {};
     for (var i = 0; i < country_codes.length; i++) {
         codes[country_codes[i][2]] = country_codes[i][1];
@@ -32,18 +37,83 @@ d3.csv("data/suicide_by_country.csv", function(error, data) {
 
     console.log(data);
 
+    colorScale = d3.scale.quantile()
+        .domain(color_values)
+        .range(all_colors);
+
     // create dataset in map format
     data.forEach(function(d) {
 
         d.female = +d.female;
         d.male = +d.male;
         d.suicide = +d.suicide;
-        // console.log(d.country);// console.log(Country)
+        var iso = codes[d.country];
+        dataset[iso] = {
+            suicide: d.suicide,
+            male: d.male,
+            female: d.female,
+            fillColor: colorScale(d.suicide)
+        };
     });
 
     var worldmap = new Datamap({
-      element: document.getElementById("mapcontainer")
+        element: document.getElementById("mapcontainer"),
+    fills: {
+      // fill for countries without data
+      defaultFill: '#e2e2e2'
+    },
+    data: dataset
     });
+
+    // set up attributes for legend
+    var legendWidth = 110,
+        legendHeight = 500;
+
+    // create svg for legend
+    var svg = d3.select(".datamap").append("svg")
+        .attr("width", legendWidth)
+        .attr("height", legendHeight)
+        .attr("id", "legendContainer");
+
+    var legend = svg.selectAll("g.legend")
+        .data(color_values)
+        .enter().append("g")
+        .attr("class", "legend");
+
+    // width and height for rects of legend
+    var r_width = 20,
+        r_height = 20;
+
+
+    var legendLabels = ["< 3", "3 - 5", "5 - 7", "7 - 10", "10 - 15", "15 - 20", "> 20"];
+    legendLabels.reverse();
+    makeLegend();
+
+    function makeLegend() {
+        color_values.reverse();
+
+        // add containers for legend
+        legend.append("rect")
+            .attr("x", 20)
+            .attr("y", function(d, i) { return legendHeight - (i * r_height) - 2 * r_height; })
+            .attr("width", r_width)
+            .attr("height", r_height)
+            .style("fill", function(d, i) { return colorScale(color_values[i]); })
+            .style("opacity", 0.8);
+
+        // add labels to legend
+        legend.append("text")
+            .attr("x", 45)
+            .attr("y", function(d, i) { return legendHeight - (i * r_height) - r_height - 5; })
+            .text(function(d, i) { return legendLabels[i]; });
+
+        // // inital legend title
+        // svg.append("text")
+        //     .attr("id", "legendTitle")
+        //     .attr("x", 20)
+        //     .attr("y", 290)
+        //     .text("Suicide");
+    }
 
     // The table generation function
     function makeTable(data, columns) {
